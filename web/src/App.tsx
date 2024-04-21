2b591f2c23694ee3a9b88bf36cb42faa90a8d666
Harsheel Singh <hsin212@aucklanduni.ac.nz>
2024-04-22 11:54:09 +1200

    UADS-15 feat: Setup all initial pages and frontend routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,37 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { useState } from 'react';
 import Home from '@pages/Home';
+import About from '@pages/About';
+import Event from '@pages/Event';
+import Sponsor from '@pages/Sponsor';
+import Login from '@pages/Login';
+import SignUp from '@pages/Signup';
+import Setting from '@pages/Setting';
 
 const router = createBrowserRouter([
   {
-    path: '/:name',
-    element: <Home />,
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/events',
+    element: <Event />,
+  },
+  {
+    path: '/sponsors',
+    element: <Sponsor />,
+  },
+  {
+    path: '/settings',
+    element: <Setting />,
   },
 ]);
 
---
+++ web/src/pages/Sponsor.tsx
@@ -0,0 +1,8 @@
+export default function Sponsor() {
+    return (
+      <div>
+        <h1> Sponsors Page</h1>
+      </div>
+    )
+}
+  
