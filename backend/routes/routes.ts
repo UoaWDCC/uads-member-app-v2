7dfc374b329d2b893a8517e494353287d5ea0cbb
Kai Hirafune <khir829@aucklanduni.ac.nz>
2024-04-23 22:06:31 +1200

    UADS-41 setup: reconfigure database schema

--- backend/routes/routes.ts
+++ backend/routes/routes.ts
@@ -1,12 +1,8 @@
 import express from "express";
-import userRoutes from "./userRoutes";
 import eventRoutes from "./eventRoutes";
 import sponsorRoutes from "./sponsorRoutes";
 
 const router = express.Router();
-
-// All user routes
-router.use("/api/user", userRoutes);
 
 // All event routes
 router.use("/api/events", eventRoutes);
