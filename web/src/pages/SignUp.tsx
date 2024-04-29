e10e7a01622c858c63d7aa842419635129eaaeae
nwil850 <nwil850@aucklanduni.ac.nz>
2024-04-29 17:01:10 +1200

    UADS-17 feat: Added navbar to sponsor and signup

--- web/src/pages/SignUp.tsx
+++ web/src/pages/SignUp.tsx
@@ -1,6 +1,9 @@
+import Navbar from "../Components/Navbar";
+
 export default function SignUp() {
     return (
       <div>
+        <Navbar />
         <h1> Sign Up Page</h1>
       </div>
     )
--- web/src/pages/Sponsor.tsx
+++ web/src/pages/Sponsor.tsx
@@ -1,6 +1,9 @@
+import Navbar from "../Components/Navbar"
+
 export default function Sponsor() {
     return (
       <div>
+        <Navbar/>
         <h1> Sponsors Page</h1>
       </div>
     )
