a667ed1258d99d3634c9c9652399f2aac439c492
nadiaaskari <nask472@aucklanduni.ac.nz>
2024-04-29 17:01:01 +1200

    UADS-17 feat: added nav bar to about us and events page

--- web/src/pages/About.tsx
+++ web/src/pages/About.tsx
@@ -1,6 +1,9 @@
+import Navbar from "../Components/Navbar";
+
 export default function About()  {
   return (
     <div>
+      <Navbar />
       <h1> About Us Page</h1>
     </div>
   )
--- web/src/pages/Event.tsx
+++ web/src/pages/Event.tsx
@@ -1,6 +1,9 @@
+import Navbar from "../Components/Navbar";
+
 export default function Event() {
   return (
     <div>
+      <Navbar />
       <h1> Events Page</h1>
     </div>
   )
