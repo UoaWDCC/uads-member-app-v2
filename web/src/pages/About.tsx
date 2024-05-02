49acdfe1f7bad89c9c257cd9020c93f52782cae8
Aye Thu <athu157@aucklanduni.ac.nz>
2024-05-02 19:49:47 +1200

    UADS-45 feat: Adding the about information

--- web/src/pages/About.tsx
+++ web/src/pages/About.tsx
@@ -2,6 +2,10 @@ export default function About()  {
   return (
     <div>
       <h1> About Page</h1>
+			<p> The Dessert Society aims to bring people together through the joy of dessert. </p>
+			<p>	With many different types of events, from bake-offs to cultural food events, we create an environment that anyone can join and have their sweet tooth fulfilled.</p>
+      <p> As a member, you gain access to discounts and deals at our sponsored dessert hotspots. </p>
     </div>
+    
   )
 }
