211b36f71b96c6414a47959fbd5f287ef1ef055e
nadiaaskari <nask472@aucklanduni.ac.nz>
2024-05-02 14:13:12 +1200

    UADS-17 style: Started styling the navigation bar

--- web/src/Components/Navbar.tsx
+++ web/src/Components/Navbar.tsx
@@ -1,22 +1,68 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-      <div>
-        <NavLink to="/about">
+    <nav className="flex items-center px-25 p-5 bg-[#E6C3BE] uppercase space-x-4">
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          `font-bold py-2 px-4 transition-all duration-300 ml-4 ${
+            isActive
+              ? "text-[#E6C3BE] bg-[#3D3D3F] rounded-full"
+              : "text-[#3D3D3F] hover:bg-[#3D3D3F] hover:text-[#E6C3BE] hover:rounded-full"
+          }`
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) =>
+          `font-bold py-2 px-4 transition-all duration-300 ${
+            isActive
+              ? "text-[#E6C3BE] bg-[#3D3D3F] rounded-full"
+              : "text-[#3D3D3F] hover:bg-[#3D3D3F] hover:text-[#E6C3BE] hover:rounded-full"
+          }`
+        }
+      >
         About
-        </NavLink>
-        <NavLink to="/events">
+      </NavLink>
+      <NavLink
+        to="/events"
+        className={({ isActive }) =>
+          `font-bold py-2 px-4 transition-all duration-300 ${
+            isActive
+              ? "text-[#E6C3BE] bg-[#3D3D3F] rounded-full"
+              : "text-[#3D3D3F] hover:bg-[#3D3D3F] hover:text-[#E6C3BE] hover:rounded-full"
+          }`
+        }
+      >
         Events
-        </NavLink>
-        <NavLink to="/sponsors">
+      </NavLink>
+      <NavLink
+        to="/sponsors"
+        className={({ isActive }) =>
+          `font-bold py-2 px-4 transition-all duration-300 ${
+            isActive
+              ? "text-[#E6C3BE] bg-[#3D3D3F] rounded-full"
+              : "text-[#3D3D3F] hover:bg-[#3D3D3F] hover:text-[#E6C3BE] hover:rounded-full"
+          }`
+        }
+      >
         Sponsors
-        </NavLink>
-        <NavLink to="/signup">
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={({ isActive }) =>
+          `font-bold py-2 px-4 transition-all duration-300 ${
+            isActive
+              ? "text-[#E6C3BE] bg-[#3D3D3F] rounded-full"
+              : "text-[#3D3D3F] hover:bg-[#3D3D3F] hover:text-[#E6C3BE] hover:rounded-full"
+          }`
+        }
+      >
         Signup
-        </NavLink>
-        
-
-      </div>
-  )
+      </NavLink>
+    </nav>
+  );
 }
