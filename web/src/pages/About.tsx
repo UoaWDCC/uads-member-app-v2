51e92878e1d1659a6dc694038148f6c7180a9f78
Aye Thu <athu157@aucklanduni.ac.nz>
2024-05-02 23:06:13 +1200

    UADS-45 feat: Created the basic skeleton

--- web/src/pages/About.tsx
+++ web/src/pages/About.tsx
@@ -1,11 +1,37 @@
-export default function About()  {
-  return (
-    <div>
-      <h1> About Page</h1>
-			<p> The Dessert Society aims to bring people together through the joy of dessert. </p>
-			<p>	With many different types of events, from bake-offs to cultural food events, we create an environment that anyone can join and have their sweet tooth fulfilled.</p>
-      <p> As a member, you gain access to discounts and deals at our sponsored dessert hotspots. </p>
-    </div>
-    
-  )
+
+
+export default function About() {
+	return (
+		<div className="w-screen h-screen bg-[#FFBBBB] flex justify-center items-center p-40">
+			<div className="flex flex-col items-center">
+				<h1 className="text-5xl text-center text-[#5F4444] mb-10 font-bold"> What we do</h1>
+				{/* Add UADS logo */}
+				{/* <p>ADD UADS LOGO</p> */}
+
+				<p className="text-2xl text-center text-[#5F4444] font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ex cum animi labore enim, error dolorem explicabo modi, dolorum est quidem perferendis blanditiis quis nemo, sunt veritatis voluptates dolores inventore?</p>
+
+			</div>
+
+			<div className="fixed bottom-10 w-96 h-10 flex justify-between ">
+				<div className="w-10 h-full bg-[#5F4444] rounded-full flex justify-center items-center">
+					<p>F</p>
+				</div>
+
+				<div className="w-10 h-full bg-[#5F4444]  rounded-full flex justify-center items-center">
+					<p>F</p>
+				</div>
+				<div className="w-10 h-full bg-[#5F4444]  rounded-full flex justify-center items-center">
+					<p>F</p>
+				</div>
+				<div className="w-10 h-full bg-[#5F4444]  rounded-full flex justify-center items-center">
+					<p>F</p>
+				</div>
+				<div className="w-10 h-full bg-[#5F4444]  rounded-full flex justify-center items-center">
+					<p>F</p>
+				</div>
+			</div>
+
+		</div>
+
+	)
 }
