dbac079d28b6d316b5af8881913e7ee9b4661b73
nwil850 <nwil850@aucklanduni.ac.nz>
2024-05-06 16:37:32 +1200

    UADS-17 style: Changed text font and styling/position of navbar

--- web/src/Components/Navbar.tsx
+++ web/src/Components/Navbar.tsx
@@ -1,63 +1,71 @@
 import { NavLink } from "react-router-dom";
+import logo from "../assets/UADS_Logo.svg"
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center px-20 py-4 bg-[#E6C3BE] uppercase space-x-4">
-      <NavLink
-        to="/"
-        className={({ isActive }) =>
-          `font-inter font-semibold text-20px text-normal uppercase py-2 px-4 transition-all duration-300 ml-4 ${isActive
-          ? "text-[#E6C3BE] bg-[#3D3D3F] rounded-full"
-            : "text-[#3D3D3F] hover:bg-[#3D3D3F] hover:text-[#E6C3BE] hover:rounded-full"
-          }`
-        }
-      >
-        <span className="px-3 select-none">Home</span>
-      </NavLink>
-      <NavLink
-        to="/about"
-        className={({ isActive }) =>
-          `font-inter font-semibold text-20px text-normal uppercase py-2 px-4 transition-all duration-300 ${isActive
-          ? "text-[#E6C3BE] bg-[#3D3D3F] rounded-full"
-            : "text-[#3D3D3F] hover:bg-[#3D3D3F] hover:text-[#E6C3BE] hover:rounded-full"
-          }`
-        }
-      >
-        <span className="px-3 select-none">About Us</span>
-      </NavLink>
-      <NavLink
-        to="/events"
-        className={({ isActive }) =>
-          `font-inter font-semibold text-20px text-normal uppercase py-2 px-4 transition-all duration-300 ${isActive
-          ? "text-[#E6C3BE] bg-[#3D3D3F] rounded-full"
-            : "text-[#3D3D3F] hover:bg-[#3D3D3F] hover:text-[#E6C3BE] hover:rounded-full"
-          }`
-        }
-      >
-        <span className="px-3 select-none">Events</span>
-      </NavLink>
-      <NavLink
-        to="/sponsors"
-        className={({ isActive }) =>
-          `font-inter font-semibold text-20px text-normal uppercase py-2 px-4 transition-all duration-300 ${isActive
-          ? "text-[#E6C3BE] bg-[#3D3D3F] rounded-full"
-            : "text-[#3D3D3F] hover:bg-[#3D3D3F] hover:text-[#E6C3BE] hover:rounded-full"
-          }`
-        }
-      >
-        <span className="px-3 select-none">Sponsors</span>
-      </NavLink>
+    <nav className="flex items-center px-20 py-4 bg-[#E6C3BE] uppercase min-w-[900px] justify-between">
+      <div className = "h-7 w-7">
+        <img src={logo} alt="adi"/>
+      </div>
+      <div className="bg-black"> 
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
+              ? "text-[#FCBA04]"
+              : "text-[#3D3D3F]  hover:text-[#FCBA04] "
+            }`
+          }
+        >
+          <span className="px-3 select-none text-transform: lowercase">Home</span>
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
+              ? "text-[#FCBA04]"
+              : "text-[#3D3D3F] hover:text-[#FCBA04] "
+            }`
+          }
+        >
+          <span className="px-3 select-none text-transform: lowercase">About Us</span>
+        </NavLink>
+        <NavLink
+          to="/events"
+          className={({ isActive }) =>
+            `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
+              ? "text-[#FCBA04]"
+              : "text-[#3D3D3F] hover:text-[#FCBA04]"
+            }`
+          }
+        >
+          <span className="px-3 select-none text-transform: lowercase">Events</span>
+        </NavLink>
+        <NavLink
+          to="/sponsors"
+          className={({ isActive }) =>
+            `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
+              ? "text-[#FCBA04] "
+              : "text-[#3D3D3F] hover:text-[#FCBA04]"
+            }`
+          }
+        >
+          <span className="px-3 select-none text-transform: lowercase">Sponsors</span>
+        </NavLink>
+      </div>
+      <div className="bg-pink">
       <NavLink
         to="/signup"
         className={({ isActive }) =>
-          `font-inter font-semibold text-20px text-normal uppercase py-2 px-4 transition-all duration-300 ${isActive
-          ? "text-[#E6C3BE] bg-[#3D3D3F] rounded-full"
-            : "text-[#3D3D3F] hover:bg-[#3D3D3F] hover:text-[#E6C3BE] hover:rounded-full"
+          `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
+            ? "text-[#FCBA04] "
+            : "text-[#3D3D3F]  hover:text-[#FCBA04] "
           }`
         }
       >
-        <span className="px-3 select-none">Sign Up</span>
+        <span className="px-3 select-none text-transform: capitalize">Join</span>
       </NavLink>
+      </div>
     </nav>
   );
 }
