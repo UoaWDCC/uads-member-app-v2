b7f03b0d0e3ade292b548f630361f06c03ed62bd
nwil850 <nwil850@aucklanduni.ac.nz>
2024-05-12 19:08:02 +1200

    UADS-17 Style: Edited navbar almost finished, still need to add swirls.

--- web/src/Components/Navbar.tsx
+++ web/src/Components/Navbar.tsx
@@ -3,54 +3,54 @@ import logo from "../assets/UADS_Logo.svg"
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center px-20 py-4 bg-[#E6C3BE] uppercase min-w-[900px] justify-between">
+    <nav className="flex items-center px-20 py-4 bg-[#B04F61] uppercase min-w-[900px] justify-between">
       <div className = "h-7 w-7">
-        <img src={logo} alt="adi"/>
+        <img src={logo} alt="UADS_Logo"/>
       </div>
-      <div className="bg-black"> 
+      <div className=""> 
         <NavLink
           to="/"
           className={({ isActive }) =>
             `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
               ? "text-[#FCBA04]"
-              : "text-[#3D3D3F]  hover:text-[#FCBA04] "
+              : "text-[#E6C3BE]  hover:text-[#FCBA04] "
             }`
           }
         >
-          <span className="px-3 select-none text-transform: lowercase">Home</span>
+          <span className="px-3 select-none text-transform: capitalize">Home</span>
         </NavLink>
         <NavLink
           to="/about"
           className={({ isActive }) =>
             `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
               ? "text-[#FCBA04]"
-              : "text-[#3D3D3F] hover:text-[#FCBA04] "
+              : "text-[#E6C3BE] hover:text-[#FCBA04] "
             }`
           }
         >
-          <span className="px-3 select-none text-transform: lowercase">About Us</span>
+          <span className="px-3 select-none text-transform: capitalize">About</span>
         </NavLink>
         <NavLink
           to="/events"
           className={({ isActive }) =>
             `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
               ? "text-[#FCBA04]"
-              : "text-[#3D3D3F] hover:text-[#FCBA04]"
+              : "text-[#E6C3BE] hover:text-[#FCBA04]"
             }`
           }
         >
-          <span className="px-3 select-none text-transform: lowercase">Events</span>
+          <span className="px-3 select-none text-transform: capitalize">Events</span>
         </NavLink>
         <NavLink
           to="/sponsors"
           className={({ isActive }) =>
             `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
               ? "text-[#FCBA04] "
-              : "text-[#3D3D3F] hover:text-[#FCBA04]"
+              : "text-[#E6C3BE] hover:text-[#FCBA04]"
             }`
           }
         >
-          <span className="px-3 select-none text-transform: lowercase">Sponsors</span>
+          <span className="px-3 select-none text-transform: capitalize">Sponsors</span>
         </NavLink>
       </div>
       <div className="bg-pink">
@@ -59,11 +59,11 @@ export default function Navbar() {
         className={({ isActive }) =>
           `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
             ? "text-[#FCBA04] "
-            : "text-[#3D3D3F]  hover:text-[#FCBA04] "
+            : "text-[#E6C3BE]  hover:text-[#FCBA04] "
           }`
         }
       >
-        <span className="px-3 select-none text-transform: capitalize">Join</span>
+        <span className="px-1 select-none text-transform: capitalize">Join</span>
       </NavLink>
       </div>
     </nav>
