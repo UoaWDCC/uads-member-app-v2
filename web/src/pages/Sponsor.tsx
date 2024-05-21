6ffd01976baa005d30eddedc81a7ec6e0c6fc52c
Nicholas Tan <163990283+n1cholastan@users.noreply.github.com>
2024-05-21 16:26:14 +1200

    UADS-19 feat: search bar

--- web/src/pages/Sponsor.tsx
+++ web/src/pages/Sponsor.tsx
@@ -3,7 +3,10 @@ import placeholder from "../assets/download.jpg";
 export default function Sponsor() {
     return (
       <>
-     <SponsorSection />
+      <p className="h-20 w-full bg-red-50">
+        Placeholder navbar
+        </p>
+      <SponsorSection />
       </>
     )
 }
@@ -13,15 +16,30 @@ export default function Sponsor() {
 function SponsorSection() {
   return (
     <>
-      <div className="flex flex-col h-auto bg-brown content-center">
-        <h1 className="p-10 text-center text-6xl font-bold text-light-pink font-raleway">
-          Our Upcoming Events
-        </h1>
-        {/* event group cards not centered */}
-        <SponsorGroups />
+      <div className="flex flex-col h-auto bg-light-pink content-center">
+        <div className="mx-20">
+          <h1 className="my-10 text-6xl font-bold text-brown font-raleway">
+            Our Sponsors
+          </h1>
+          <SearchBar />
+          {/* event group cards not centered */}
+          <SponsorGroups />
+        </div>
       </div>
     </>
   );
+}
+
+function SearchBar() {
+  return (
+    <>
+      <div className="pt-2 mx-auto text-gray-600 w-full">
+        <input className="bg-white h-14 px-5 pr-16 rounded-xl text-lg focus:outline-none font-raleway font-thin justify-center w-full" type="search" name="search" placeholder="Search Sponsors..."></input>
+        <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
+        </button>
+      </div>
+    </>
+  )
 }
 
 function SponsorGroups() {
