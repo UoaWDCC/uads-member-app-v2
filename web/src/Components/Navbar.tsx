5f06489397bdce68e150c7925392deb6daeca5f1
Harsheel Singh <hsin212@aucklanduni.ac.nz>
2024-05-22 23:17:05 +1200

    UADS-17 refactor: Removed unnecessary divs

--- web/src/Components/Navbar.tsx
+++ web/src/Components/Navbar.tsx
@@ -1,78 +1,40 @@
 import { NavLink } from "react-router-dom";
 import logo from "../assets/UADS_Logo.svg";
+import topRight from "../assets/topright.png";
 import BackgroundSVG from "../assets/sin.svg";
-import joinImage from "../assets/joinimage.svg"
+import joinImage from "../assets/joinimage.svg";
 
 export default function Navbar() {
-  return (
-    <div className="relative">
-      <div>
-          <img src={BackgroundSVG} alt="sin.svg" className="mt-[-2.5%]" width="100%"/>
-      </div>
-      <nav className="absolute inset-0 flex items-center px-12 py-1 bg-transparent uppercase min-w-[900px] justify-between">
-        <div>
-          <img src={logo} alt="UADS_Logo" width="100" height="100"/>
-        </div>
-        <div> 
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
-              ? "text-yellow"
-              : "text-light-pink  hover:text-yellow "
-            }`
-          }
-        >
-          <span className="px-3 text-2xl select-none text-transform: capitalize">Home</span>
-        </NavLink>
-        <NavLink
-          to="/about"
-          className={({ isActive }) =>
-            `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
-              ? "text-yellow"
-              : "text-light-pink hover:text-yellow "
-            }`
-          }
-        >
-          <span className="px-3 text-2xl select-none text-transform: capitalize">About</span>
-        </NavLink>
-        <NavLink
-          to="/events"
-          className={({ isActive }) =>
-            `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
-              ? "text-yellow"
-              : "text-light-pink hover:text-yellow"
-            }`
-          }
-        >
-          <span className="px-3 text-2xl select-none text-transform: capitalize">Events</span>
-        </NavLink>
-        <NavLink
-          to="/sponsors"
-          className={({ isActive }) =>
-            `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
-              ? "text-yellow "
-              : "text-light-pink hover:text-yellow"
-            }`
-          }
-        >
-          <span className="px-3 text-2xl select-none text-transform: capitalize">Sponsors</span>
-        </NavLink>
-      </div>
-      <div className="">
-      <NavLink
-        to="/signup"
-        className={({ isActive }) =>
-          `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive
-            ? "text-yellow "
-            : "text-light-pink  hover:text-yellow "
-          }`
-        }
-      >
-        <span className="px-1 text-2xl select-none text-transform: capitalize">Join</span>
-      </NavLink>
-      </div>
-      </nav>
-    </div>
-  );
+	return (
+		<div className="max-w-screen h-24 bg-pink relative px-10">
+			<div className="relative z-10 flex justify-center items-center h-full">
+				<div className="w-full h-auto flex justify-between items-center">
+					<img src={logo} alt="UADS Logo" />
+
+					<div className="flex justify-between w-[40rem] text-2xl select-none">
+						<NavLink to="/" className={({ isActive }) => `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive ? "text-yellow" : "text-light-pink  hover:text-yellow "}`}>
+							Home
+						</NavLink>
+						<NavLink to="/about" className={({ isActive }) => `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive ? "text-yellow" : "text-light-pink  hover:text-yellow "}`}>
+							About
+						</NavLink>
+						<NavLink to="/events" className={({ isActive }) => `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive ? "text-yellow" : "text-light-pink  hover:text-yellow "}`}>
+							Events
+						</NavLink>
+						<NavLink to="/sponsors" className={({ isActive }) => `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive ? "text-yellow" : "text-light-pink  hover:text-yellow "}`}>
+							Sponsors
+						</NavLink>
+					</div>
+
+					<div className="text-2xl select-none relative h-full">
+						<NavLink to="/signup" className={({ isActive }) => `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive ? "text-yellow" : "text-light-pink  hover:text-yellow "}`}>
+							Join
+						</NavLink>
+					</div>
+				</div>
+			</div>
+
+			<img src={topRight} alt="background" className="h-full absolute top-0 right-0" />
+		</div>
+	);
 }
