2f0dd441c996ea98afd403f7615378e455f5a8d6
Nicholas Tan <163990283+n1cholastan@users.noreply.github.com>
2024-05-22 14:23:40 +1200

    UADS-19 feat: basic sponsorcard

--- web/src/pages/Sponsor.tsx
+++ web/src/pages/Sponsor.tsx
@@ -1,4 +1,5 @@
 import placeholder from "../assets/download.jpg";
+import donut from "../assets/dessert-donut-doughnut-svgrepo-com 1.svg";
 
 export default function Sponsor() {
     return (
@@ -17,15 +18,17 @@ function SponsorSection() {
   return (
     <>
       <div className="flex flex-col h-auto bg-light-pink content-center">
-        <div className="mx-20">
-          <h1 className="my-10 text-6xl font-bold text-brown font-raleway">
+        <div className="mx-20 z-10">
+          <h1 className="mt-10 mb-5 text-6xl font-bold text-brown font-raleway">
             Our Sponsors
           </h1>
           <SearchBar />
           {/* event group cards not centered */}
           <SponsorGroups />
         </div>
+        <img className="absolute md:top-32 md:right-36 md:w-36 invisible md:visible " src={donut}></img>
       </div>
+      
     </>
   );
 }
@@ -34,9 +37,7 @@ function SearchBar() {
   return (
     <>
       <div className="pt-2 mx-auto text-gray-600 w-full">
-        <input className="bg-white h-14 px-5 pr-16 rounded-xl text-lg focus:outline-none font-raleway font-thin justify-center w-full" type="search" name="search" placeholder="Search Sponsors..."></input>
-        <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
-        </button>
+        <input className="bg-white h-14 px-5 rounded-xl text-lg focus:outline-none font-raleway font-thin justify-center w-full" type="search" name="search" placeholder="Search Sponsors..."></input>
       </div>
     </>
   )
@@ -45,7 +46,7 @@ function SearchBar() {
 function SponsorGroups() {
   return (
     <>
-      <div className=" flex flex-wrap justify-start m-10">
+      <div className=" flex flex-wrap justify-between my-10">
         <SponsorCard
           image={placeholder}
           sponsorName="Sparkle Gala Dinner"
@@ -53,13 +54,8 @@ function SponsorGroups() {
         />
         <SponsorCard
           image={placeholder}
-          sponsorName="Sparkle Gala Dinner"
-          sponsorDesc="Come and enjoy this dinner where our sponsors will be present"
-        />
-        <SponsorCard
-          image={placeholder}
-          sponsorName="Sparkle Gala Dinner"
-          sponsorDesc="Come and enjoy this dinner where our sponsors will be present"
+          sponsorName="Tsujiri"
+          sponsorDesc="Free upsize and free topping"
         />
       </div>
     </>
@@ -79,21 +75,20 @@ function SponsorCard({
 }: SponsorCardProps) {
   return (
     <>
-        
-            <div className="h-64 overflow-hidden">
-              <img
-                className=" rounded-3xl rounded-b-none"
-                src={image}
-                
-              />
-            <div className="flex flex-row justify-evenly m-5 pb-5">
-            
-                <h2 className="text-center md:text-left font-bold text-light-pink text-xl font-raleway">
-                  {sponsorName}
-                </h2>
-                <p className="text-light-pink text-sm font-raleway">{sponsorDesc}</p>
-            </div>
-          </div>
+      <div className="bg-pink rounded-2xl flex flex-row p-8 min-w-fit">
+        <div className="w-36 h-36 align-middle flex overflow-hidden">
+          <img className="rounded-xl align-center aspect-auto w-fit" src={image}
+          />
+        </div>
+        <div className="px-5">
+          <h1 className="text-light-pink font-raleway text-2xl font-bold truncate">
+            {sponsorName}
+          </h1>
+          <p className="text-light-pink font-raleway text-lg font-medium truncate">
+            {sponsorDesc}
+          </p>
+        </div>
+      </div>
     </>
   );
 }
