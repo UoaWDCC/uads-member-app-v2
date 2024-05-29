babb8c68555b21d87615363f0b48fc6e324abace
nadiaaskari <nask472@aucklanduni.ac.nz>
2024-05-29 23:13:15 +1200

    UADS-20 feat: Implemted basic layout for events page

---
+++ web/src/Components/EventCard.tsx
@@ -0,0 +1,25 @@
+export default function EventCard() {
+
+  return (
+   <>
+        <div className="w-72 h-64 bg-pink rounded-3xl">
+            <div className="h-[57%] bg-green-300 rounded-t-3xl">
+                
+            </div>
+
+            <div className="w-full h-[43%] flex rounded-3xl bg-blue-200">
+                <div className="w-1/4 h-auto bg-blue-500">
+                
+                </div>
+                
+                <div className="">
+                
+                </div>
+                
+            </div>
+
+
+        </div>
+   </>
+  );
+}
--- web/src/pages/Event.tsx
+++ web/src/pages/Event.tsx
@@ -1,10 +1,36 @@
 import Navbar from "../Components/Navbar";
+import cupcake from "../assets/cupcake.svg";
+import EventCard from "../Components/EventCard";
 
 export default function Event() {
   return (
-    <div>
+    <>
       <Navbar />
-      <h1> Events Page</h1>
-    </div>
-  )
+
+      <div className="max-w-screen h-auto bg-light-pink py-8 px-14">
+        <div className="w-full h-auto my-10 flex flex-col ">
+          <div className="flex justify-between">
+          <h1 className="text-5xl font-bold font-raleway text-brown pt-10">Events</h1>
+          
+          <div className="items-end">
+            <img className="" src={cupcake} alt="Cupcake" />
+          </div>
+          </div>
+          
+
+          <div className="w-full flex justify-center">
+            <input
+              type="text"
+              className="w-full h-16 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 px-5"
+              placeholder="Search Events.."
+            />
+          </div>
+        </div>
+        
+
+        <EventCard />
+        
+      </div>
+    </>
+  );
 }
