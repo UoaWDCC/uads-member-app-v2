124a1ef18da92a5da9d28f54458c0559c5cc6a51
nwil850 <nwil850@aucklanduni.ac.nz>
2024-06-05 00:29:08 +1200

    UADS:17 style: Added hamburger icon and functionality

--- web/src/Components/Navbar.tsx
+++ web/src/Components/Navbar.tsx
@@ -1,78 +1,89 @@
 import { NavLink } from "react-router-dom";
 import logo from "../assets/UADS_Logo.svg";
-import topRight from "../assets/topright.png";
+import brownBackground from "../assets/brownBackground.png";
 import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const navlinks = [
-  {
-    title: "Home",
-    link: "/",
-  },
-  {
-    title: "About",
-    link: "/about",
-  },
-  {
-    title: "Events",
-    link: "/events",
-  },
-  {
-    title: "Sponsors",
-    link: "/sponsors",
-  },
+	{
+		title: "Home",
+		link: "/",
+	},
+	{
+		title: "About",
+		link: "/about",
+	},
+	{
+		title: "Events",
+		link: "/events",
+	},
+	{
+		title: "Sponsors",
+		link: "/sponsors",
+	},
 ];
 
 export default function Navbar() {
-  const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-  const handleMenu = () => {
-    setOpen((prev) => !prev);
-    console.log(open);
-  };
+	const handleMenu = () => {
+		setOpen((prev) => !prev);
+	};
 
-  return (
-    <div className="max-w-screen h-24 bg-pink relative px-10">
-      <div className="relative z-10 flex justify-center items-center h-full">
-        <div className="w-full h-auto flex justify-between items-center select-none">
-          <NavLink to={"/"}>
-            <img src={logo} alt="UADS Logo" />
-          </NavLink>
-          {/* Navlinks */}
-          <div className="justify-between text-2xl hidden lg:block">
-            {navlinks.map((link, index) => (
-              <NavLink key={index} className={({ isActive }) => `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive ? "text-yellow" : "text-light-pink  hover:text-yellow "}`} to={link.link}>
-                {link.title}
-              </NavLink>
-            ))}
-          </div>
-          <div className="text-2xl select-none relative h-full hidden lg:block">
-            <NavLink className={({ isActive }) => `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive ? "text-yellow" : "text-light-pink  hover:text-yellow "}`} to={"/signup"}>
-              Join
-            </NavLink>
-          </div>
-          {/* Hamburger button */}
-          <div className="-mr-2 lg:hidden z-0">
-            <button type="button" onClick={handleMenu} className="inline-flex items-center justify-center p-2 rounded-lg text-light-pink hover:text-yellow">
-              <span className="sr-only">Open Main Menu</span>
-              {open ? <FaTimes /> : <FaBars />}
-            </button>
-          </div>
-          {/* Mobile Menu */}
-          {open ? (
-            <div className="lg:hidden">
-              <div className="box-100% pt-40 space-y-1 z-100 ">
-                {navlinks.map((link, index) => (
-                  <NavLink key={index} className={({ isActive }) => `block px-3 py-1 bg-pink font-raleway transition-all duration-300 font-bold ${isActive ? "text-yellow" : "text-light-pink  hover:text-yellow "}`} to={link.link}>
-                    {link.title}
-                  </NavLink>
-                ))}
-              </div>
-            </div>
-          ) : null}
-        </div>
-      </div>
-      <img src={topRight} alt="background" className="h-full absolute top-0 right-0" />
-    </div>
-  );
+	return (
+		<div className="max-w-screen h-24 bg-pink relative px-10">
+			<div className="relative z-10 flex justify-center items-center h-full">
+				<div className="w-full h-auto flex justify-between items-center select-none">
+					<NavLink to={"/"}>
+						<img src={logo} alt="UADS Logo" />
+					</NavLink>
+					{/* Navlinks */}
+					<div className="hidden lg:flex justify-between text-2xl">
+						{navlinks.map((link, index) => (
+							<NavLink key={index} className={({ isActive }) => `font-raleway px-10 transition-all duration-300 font-bold ${isActive ? "text-yellow" : "text-light-pink  hover:text-yellow "}`} to={link.link}>
+								{link.title}
+							</NavLink>
+						))}
+					</div>
+					<div className="hidden lg:flex text-2xl select-none relative h-full">
+						<NavLink className={({ isActive }) => `font-raleway py-2 px-4 transition-all duration-300 font-bold ${isActive ? "text-yellow" : "text-light-pink  hover:text-yellow "}`} to={"/signup"}>
+							Join
+						</NavLink>
+					</div>
+
+					{/* Hamburger button */}
+					<div className="lg:hidden">
+						<button type="button" onClick={handleMenu} className="inline-flex items-center justify-center  rounded-lg text-light-pink hover:text-yellow">
+							<FaBars size={30} color="pink" />
+						</button>
+					</div>
+				</div>
+			</div>
+
+			{/* Mobile Menu */}
+			{open && (
+				<div className="lg:hidden absolute top-0 right-0 w-full bg-pink pt-20 pb-4 z-10 flex flex-col items-center">
+					<button type="button" onClick={handleMenu} className="absolute top-7 right-10">
+						<FaTimes size={30} color="pink" />
+					</button>
+
+					<div className="absolute top-4 left-10">
+						<NavLink to={"/"}>
+							<img src={logo} alt="UADS Logo" />
+						</NavLink>
+					</div>
+
+					{navlinks.map((link, index) => (
+						<NavLink key={index} className={({ isActive }) => `block py-4 bg-pink font-raleway transition-all duration-300 font-bold ${isActive ? "text-yellow" : "text-light-pink  hover:text-yellow "}`} to={link.link} onClick={() => setOpen(false)}>
+							{link.title}
+						</NavLink>
+					))}
+					<NavLink className={({ isActive }) => `block py-4 bg-pink font-raleway transition-all duration-300 font-bold ${isActive ? "text-yellow" : "text-light-pink  hover:text-yellow "}`} to={"/signup"} onClick={() => setOpen(false)}>
+						Join
+					</NavLink>
+				</div>
+			)}
+			<img src={brownBackground} alt="background" className="h-full absolute top-0 right-0" />
+		</div>
+	);
 }
