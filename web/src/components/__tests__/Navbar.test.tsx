c5f37f812deeb2d725d3a6337e32f97e7c5d0dd2
Harsheel Singh <hsin212@aucklanduni.ac.nz>
2024-06-12 21:56:03 +1200

    UADS-17 refactor: Removed unnecessary images

--- web/src/components/__tests__/Navbar.test.tsx
+++ web/src/components/__tests__/Navbar.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import Navbar from "../Navbar";
+import Navbar from "../../components/Navbar";
 import "@testing-library/jest-dom";
 
 describe("Navbar Component", () => {
