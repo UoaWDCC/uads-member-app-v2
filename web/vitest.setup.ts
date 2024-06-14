48166f577a3c54321a42e2f3d1d995a6cadbe349
Kai Hirafune <khir829@aucklanduni.ac.nz>
2024-06-14 14:31:18 +1200

    UADS-18 test: add additional setup for tests

--- web/vitest.setup.ts
+++ web/vitest.setup.ts
@@ -1,1 +1,14 @@
 import "@testing-library/jest-dom";
+import "regenerator-runtime/runtime";
+/**
+ * fix: `matchMedia` not present, legacy browsers require a polyfill
+ */
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
