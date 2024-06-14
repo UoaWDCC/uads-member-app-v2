991727adfbef10c0b41f213f5afa8292ba21d50f
Harsheel Singh <hsin212@aucklanduni.ac.nz>
2024-06-15 03:15:49 +1200

    UADS-45 test: Added test cases for About Page

--- web/src/pages/__tests__/About.test.tsx
+++ web/src/pages/__tests__/About.test.tsx
@@ -1,22 +1,42 @@
 import { render, screen } from "@testing-library/react";
-import About from "../About";
 import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
+import About from "../About";
+import icecream from "../../assets/ice-cream.svg";
 
 /*
  * Test suite to test if the elements in the About Page are rendered correctly
  */
-describe("About Page Rendering", () => {
-	it("Should display the About page title", () => {
-		// Render the About component in a simulated DOM
+describe("About Page", () => {
+	beforeEach(() => {
 		render(
 			<MemoryRouter>
 				<About />
 			</MemoryRouter>
 		);
+	});
 
-		const message = screen.getByText(/About Us Page/i);
+	it("Should render 'About Us' section correctly", () => {
+		// Check if 'About Us' title is rendered
+		const aboutTitle = screen.getByText(/About Us/i);
+		expect(aboutTitle).toBeInTheDocument();
 
-		expect(message).toBeInTheDocument();
+		// Check if introductory text is rendered
+		const introText = screen.getByText(/Welcome to the sweetest corner/i);
+		expect(introText).toBeInTheDocument();
+	});
+
+	it("Should render ice cream photo", () => {
+		const iceCreamImage = screen.getByAltText(/ice-cream photo/i);
+		expect(iceCreamImage).toBeInTheDocument();
+		expect(iceCreamImage).toHaveAttribute("src", icecream);
+	});
+
+	it("About section renders correctly for medium screens", () => {
+		const aboutSection = screen.getByTestId("aboutSection");
+		expect(aboutSection).toHaveClass("md:w-1/2");
+		expect(aboutSection).toHaveClass("md:mb-0");
+
+		expect(aboutSection).not.toHaveClass("lg:w-1/2");
 	});
 });
