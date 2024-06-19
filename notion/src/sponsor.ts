6a06492a12c04197da779670a8959ee7c7d4d34d
Harsheel Singh <hsin212@aucklanduni.ac.nz>
2024-06-19 15:50:00 +1200

    UADS-53 chore: Added nodemon scripts

--- notion/src/sponsor.ts
+++ notion/src/sponsor.ts
@@ -1,38 +1,40 @@
 import { Client } from "@notionhq/client";
+import { config } from "dotenv";
+config();
 
-
-const notionSecret = process.env.NOTION_SECRET
-const sponsorID = process.env.SPONSOR_DB_ID
-
+const notionSecret = process.env.NOTION_SECRET;
+const sponsorID = process.env.SPONSOR_DB_ID;
 
 const notion = new Client({
-  auth: notionSecret,
+	auth: notionSecret,
 });
 
-export default async function getData() {
+export default async function getSponsorData() {
+	if (!notionSecret || !sponsorID) {
+		throw new Error("Missing creds");
+	}
 
+	const query = await notion.databases.query({
+		database_id: sponsorID,
+	});
 
+	console.log("------------ ALL RESULTS --------------------");
+	console.log(query.results);
 
-  if (!notionSecret || !sponsorID) {
-    throw new Error("Missing creds")
-  }
+	console.log("------------ 1ST RESULT --------------------");
+	console.log(query.results[0]);
 
-  const query = await notion.databases.query({
-    database_id: sponsorID,
-  });
+	console.log("------------ ALL PROPERTIES --------------------");
+	// @ts-ignore
+	console.log(query.results[0].properties);
 
+	console.log("------------ Name --------------------");
+	// @ts-ignore
+	console.log(query.results[0].properties.Name.title[0].plain_text);
 
-  console.log("------------ ALL RESULTS --------------------")
-  console.log(query.results);
-
-  console.log("------------ 1ST RESULT --------------------")
-  console.log(query.results[0]);
-
-
-  console.log("------------ ALL PROPERTIES --------------------")
-  // @ts-ignore
-  console.log(query.results[0].properties);
+	console.log("------------ Description --------------------");
+	// @ts-ignore
+	console.log(query.results[0].properties.Description.rich_text[0].plain_text);
 }
 
-
-getData()
+getSponsorData();
