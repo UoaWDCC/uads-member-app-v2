b7a12a28347d99c18a253015544142d3f87637ce
Harsheel Singh <hsin212@aucklanduni.ac.nz>
2024-06-27 19:29:51 +1200

    UADS-53 feat: Integrated CMS with Sign Up Page

--- web/src/pages/SignUp.tsx
+++ web/src/pages/SignUp.tsx
@@ -1,12 +1,39 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function SignUp() {
+	const [signUpFormLink, setSignUpFormLink] = useState<string>("");
+
+	useEffect(() => {
+		async function fetchSocials() {
+			try {
+				const response = await axios.get("http://localhost:4000/api/socials/");
+				// Sign Up Link is the last index of the response body
+				setSignUpFormLink(response.data[response.data.length - 1].link);
+			} catch (error) {
+				console.error("Error fetching social data", error);
+			}
+		}
+
+		fetchSocials();
+	}, []);
+
 	return (
-		<div>
+		<>
 			<Navbar />
-			<h1> Sign Up Page</h1>
+
+			<div className="max-w-screen h-auto bg-light-pink py-10 px-5 lg:px-40">
+				<h1 className="text-4xl text-brown font-bold font-raleway">Join Us Now</h1>
+				<div className="my-5 w-full h-screen border-4 border-solid border-pink rounded-lg overflow-hidden">
+					<iframe src={signUpFormLink} width="100%" height="100%" >
+						Loading…
+					</iframe>
+				</div>
+			</div>
+
 			<Footer />
-		</div>
+		</>
 	);
 }
