420f6344476ce68a808d0a4085114fc4a898762e
Becky <beckyngan28@gmail.com>
2024-07-19 22:44:38 +1200

    feat adding event card

--- web/src/pages/Event.tsx
+++ web/src/pages/Event.tsx
@@ -1,7 +1,47 @@
+import EventCard from "../components/EventCard";
+import { EventType } from "../utils/FrontendTypes";
+
 export default function Event() {
-	return (
-		<>
-			<h1>Events Page</h1>
-		</>
-	);
+  return (
+    <>
+      <div className="max-w-screen h-auto bg-light-pink py-8 px-4 sm:px-8">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold font-raleway text-brown">
+          Events Page
+        </h1>
+        <div className="flex flex-col md:flex-row gap-x-16 p-5 sm:gap-y-16 ">
+          <div className="w-full md:w-1/3 text-center">
+            {/*  fix small padding issue  */}
+            <EventCard
+              event={{
+                name: "test",
+                date: "test",
+                description: "test",
+                image: "test",
+              }}
+            />
+          </div>
+          <div className="w-full md:w-1/3 text-center">
+            <EventCard
+              event={{
+                name: "test",
+                date: "test",
+                description: "test",
+                image: "test",
+              }}
+            />
+          </div>
+          <div className="w-full md:w-1/3 text-center">
+            <EventCard
+              event={{
+                name: "test",
+                date: "test",
+                description: "test",
+                image: "test",
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
